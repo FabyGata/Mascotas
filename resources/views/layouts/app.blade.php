f1801787746b9b7968a2446852a548401503eec0
bafy <bafy@GatoPsicopata>
2016-11-16 19:35:42 -0400

    :v

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,36 @@
 <body>
 
     <div id="header">
+
+        {{--Loggin Registrer--}}
+        <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+                <li><a href="{{ url('/login') }}">Login</a></li>
+                <li><a href="{{ url('/register') }}">Register</a></li>
+            @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ url('/logout') }}"
+                               onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            @endif
+        </ul>
+
         <a href="/" id="logo"><img src="/template_images/logo.png" width="310" height="114" alt="" title=""></a>
 
         <ul class="navigation">
@@ -38,10 +68,10 @@
     </div>
 
     {{--<nav class="navbar navbar-default navbar-static-top">--}}
-        {{--<div class="container">--}}
+        <div class="container">
             {{--<div class="navbar-header">--}}
 
-                {{--<!-- Collapsed Hamburger -->--}}
+                <!-- Collapsed Hamburger -->
                 {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
                     {{--<span class="sr-only">Toggle Navigation</span>--}}
                     {{--<span class="icon-bar"></span>--}}
@@ -49,19 +79,19 @@
                     {{--<span class="icon-bar"></span>--}}
                 {{--</button>--}}
 
-                {{--<!-- Branding Image -->--}}
+                <!-- Branding Image -->
                 {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
                     {{--{{ config('app.name', 'Laravel') }}--}}
                 {{--</a>--}}
             {{--</div>--}}
 
-            {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
-                {{--<!-- Left Side Of Navbar -->--}}
-                {{--<ul class="nav navbar-nav">--}}
-                    {{--&nbsp;--}}
-                {{--</ul>--}}
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
 
-                {{--<!-- Right Side Of Navbar -->--}}
+                <!-- Right Side Of Navbar -->
                 {{--<ul class="nav navbar-nav navbar-right">--}}
                     {{--<!-- Authentication Links -->--}}
                     {{--@if (Auth::guest())--}}
@@ -89,8 +119,8 @@
                         {{--</li>--}}
                     {{--@endif--}}
                 {{--</ul>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+            </div>
+        </div>
     {{--</nav>--}}
 
     @yield('content')
